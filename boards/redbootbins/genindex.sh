fb4d3391c550b2d88cc5a623deee8296717df499
jlarmour <jlarmour>
2001-04-07 22:54:08 +0000

    Ignore index.html files in listing Display file sizes

--- boards/redbootbins/genindex.sh
+++ boards/redbootbins/genindex.sh
@@ -6,22 +6,24 @@ for i in `find . -name CVS -prune -o -type d -print` ; do
     cat > $i/index.html <<EOF
 <HTML>
 <HEAD>
-<TITLE>RedBoot binaries and support files</TITLE>
+<TITLE>RedBoot binaries and support files for platform $i</TITLE>
 </HEAD>
 
 <body bgcolor="#FFFFFF" text="#000000" link="#CC0000" alink="#FF0000" vlink="#880000">
 
-<h1>RedBoot binaries and support files</h1>
+<h1>RedBoot binaries and support files for platform $i</h1>
 <HR noshade SIZE=3>
 <ul>
 EOF
     for j in $i/* ; do
         SHORTNAME=`echo $j | sed 's#.*/##g'`
-        if [ $SHORTNAME == "CVS" ]; then
+        if [ $SHORTNAME == "CVS" -o $SHORTNAME == index.html ]; then
             continue
         fi
+        SIZE=`ls -l $j | awk '{print $5}'`
+        SIZEK=`echo "$SIZE/1024" | bc`
         cat >> $i/index.html <<EOF
-<li><a href="$SHORTNAME">$SHORTNAME</a></li>
+<li><a href="$SHORTNAME">$SHORTNAME</a> ($SIZEK Kbytes)</li>
 EOF
     done
     cat >> $i/index.html <<EOF
