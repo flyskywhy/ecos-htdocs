624134fe2bf4ad27eaf27e4b0e25f959122f9d1f
jlarmour <jlarmour>
2001-04-07 22:55:03 +0000

    Cosmetic fixes

--- boards/redbootbins/genindex.sh
+++ boards/redbootbins/genindex.sh
@@ -6,12 +6,12 @@ for i in `find . -name CVS -prune -o -type d -print` ; do
     cat > $i/index.html <<EOF
 <HTML>
 <HEAD>
-<TITLE>RedBoot binaries and support files for platform $i</TITLE>
+<TITLE>RedBoot binaries and support files for platform `echo $i | sed 's#.*/##g'`</TITLE>
 </HEAD>
 
 <body bgcolor="#FFFFFF" text="#000000" link="#CC0000" alink="#FF0000" vlink="#880000">
 
-<h1>RedBoot binaries and support files for platform $i</h1>
+<h1>RedBoot binaries and support files for platform `echo $i | sed 's#.*/##g'`</h1>
 <HR noshade SIZE=3>
 <ul>
 EOF
